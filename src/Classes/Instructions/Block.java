1739d4565a07b53137aec171ee4cb494653fd4f9
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2024-11-11 23:40:52 -0600

    [Add] Instructions: Block

---
+++ src/Classes/Instructions/Block.java
@@ -0,0 +1,33 @@
+package Classes.Instructions;
+import java.util.ArrayList;
+import Classes.Abstracts.Instruction;
+import Classes.Abstracts.Statement;
+import Classes.Env.Env;
+import Classes.Generator.R;
+import Classes.Generator.RiscVGen;
+import Classes.Utils.TypeInst;
+public class Block extends Instruction {
+    ArrayList<Statement> instructions;
+    public Block(int line, int column, ArrayList<Statement> instructions) {
+        super(line, column, TypeInst.BLOCK_INST);
+        this.instructions = instructions;
+    }
+    public void exec(Env env, RiscVGen gen) {
+        if(env.previous == null) {
+            gen.addComment("========== BLOQUE ===========");
+
+            env.newScope();
+            this.instructions.forEach(i -> i.exec(env, gen));
+            gen.addComment("------ REDUCIENDO PILA ------");
+            final int bytesToRemove = env.endScope();
+
+            if(bytesToRemove > 0) {
+                gen.addi(R.SP.n, R.SP.n, bytesToRemove);
+            }
+
+            gen.addComment("======== FIN BLOQUE =========");
+        } else {
+            this.instructions.forEach(i -> i.exec(env, gen));
+        }
+    }
+}
