fbae61ff6eb142b53126926107f7d16e11908f22
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2024-11-14 15:38:24 -0600

    [Update] Pair

--- src/Classes/Env/Pair.java
+++ src/Classes/Env/Pair.java
@@ -1,10 +1,10 @@
 package Classes.Env;
 import Classes.Utils.StackValue;
 public class Pair {
-    public int byteOffset;
+    public int offset;
     public StackValue object;
-    public Pair(int byteOffset, StackValue object) {
-        this.byteOffset = byteOffset;
+    public Pair(int offset, StackValue object) {
+        this.offset = offset;
         this.object = object;
     }
 }
