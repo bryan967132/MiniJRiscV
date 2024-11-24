17d06b49a2bf20480df0c8fe717c47b239b30f5a
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2024-11-24 03:44:24 -0600

    [Add] Expressions: IncDec

---
+++ src/Classes/Expressions/IncDec.java
@@ -0,0 +1,30 @@
+package Classes.Expressions;
+import Classes.Abstracts.Expression;
+import Classes.Env.Env;
+import Classes.Generator.RiscVGen;
+import Classes.Instructions.Assign;
+import Classes.Utils.Type;
+import Classes.Utils.TypeExp;
+public class IncDec extends Expression {
+    private String id;
+    private String sign;
+    public IncDec(int line, int column, String id, String sign) {
+        super(line, column, sign.equals("++") ? TypeExp.INC : TypeExp.DEC);
+        this.id = id;
+        this.sign = sign;
+    }
+    public void exec(Env env, RiscVGen gen) {
+        new Assign(
+            line,
+            column,
+            id,
+            new Arithmetic(
+                line,
+                column,
+                new AccessVar(line, column, id),
+                sign.equals("++") ? "+" : "-",
+                new Primitive(line, column, "1", Type.INT)
+            )
+        ).exec(env, gen);
+    }
+}
