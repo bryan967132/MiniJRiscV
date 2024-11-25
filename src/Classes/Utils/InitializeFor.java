e23287461633d8287ea863257457856df546c700
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2024-11-24 22:59:40 -0600

    [Update] Utils

--- src/Classes/Utils/InitializeFor.java
+++ src/Classes/Utils/InitializeFor.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 import Classes.Abstracts.Instruction;
 public class InitializeFor {
     public Instruction inits;
-    public ArrayList<Instruction> asigns;
+    public ArrayList<Instruction> assigns;
     public InitializeFor(Instruction inits) {
         this.inits = inits;
     }
     public InitializeFor(ArrayList<Instruction> asigns) {
-        this.asigns = asigns;
+        this.assigns = asigns;
     }
 }
